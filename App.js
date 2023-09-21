ef469c6c70dcd2f34a43a27db58e3151d29d3273
SadzakovicFilip <filipsadzakovic.dev@gmail.com>
2023-09-21 15:16:47 +0200

    Created Root for REACT 18, and Added Keys for each element

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 const Person = (props) => {
   return React.createElement(`div`, {}, [
-    React.createElement(`h1`, {}, props.name),
-    React.createElement(`p`, {}, props.ocupation),
+    React.createElement(`h1`, {key:1}, props.name),
+    React.createElement(`p`, {key:2}, props.ocupation),
   ]);
 };
 
 const App = () => {
   return React.createElement(`div`, {}, [
-    React.createElement(`h1`, { className: "title" }, "React is Rendered"),
+    React.createElement(`h1`, { className: "title",key:3 }, "React is Rendered"),
     React.createElement(
       Person,
-      { name: `Filip`, ocupation: `Programmer` },
+      { name: `Filip`, ocupation: `Programmer`, key:4 },
       null
     ),
-    React.createElement(Person, { name: `Maja`, ocupation: `Designer` }, null),
-    React.createElement(Person, { name: `Ljilja`, ocupation: `Doctor` }, null),
+    React.createElement(Person, { name: `Maja`, ocupation: `Designer`,key:5 }, null),
+    React.createElement(Person, { name: `Ljilja`, ocupation: `Doctor`,key:6 }, null),
+    React.createElement(Person, { name: `Vera`, ocupation: `Lawyer`,key:7 }, null),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById(`root`));
+const root = ReactDOM.createRoot(document.getElementById(`root`))
+root.render(React.createElement(App))
